bdc8ecd054b9a1532c72a49db280bd4faa8dde0a
Dev-DavidPrabu <97029653+Dev-DavidPrabu@users.noreply.github.com>
2022-01-10 12:04:44 +0530

    Updated Fi Screen

---
+++ src/Pages/DiConfirmation/DiConfirmation.js
@@ -0,0 +1,226 @@
+/* eslint-disable prettier/prettier */
+import {
+  CButton,
+  CCard,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CFormLabel,
+  CFormSelect,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CRow,
+  CTabContent,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CTabPane,
+  CFormFloating,
+  CFormTextarea,
+} from '@coreui/react'
+import { React, useState } from 'react'
+import useForm from 'src/Hooks/useForm'
+import validate from 'src/Validations/FormValidation'
+import CustomTable from '../../components/customComponent/CustomTable'
+const ParkingYardGate = () => {
+  const formValues = {
+    vehicleType: '',
+    OdometerKm: '',
+    odometerPhoto: '',
+  }
+
+  const { values, errors, handleChange, onFocus, handleSubmit, enableSubmit, onBlur } = useForm(
+    login,
+    validate,
+    formValues
+  )
+
+  function login() {
+    alert('No Errors CallBack Called')
+  }
+  return (
+    <>
+      <CCard>
+        <CTabContent>
+          <CTabPane role="tabpanel" aria-labelledby="home-tab" visible={true}>
+            <CForm className="container p-3" onSubmit={handleSubmit}>
+              <CRow className="">
+                {/* <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">VA Number</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol> */}
+
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Vehicle Number</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol>
+
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Tripsheet No</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol>
+
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Vendor Name</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol>
+              {/* </CRow>
+              <CRow className=""> */}
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Vendor Code</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol>
+
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Diesel Liters</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol>
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">
+                    Invoice No.*
+                    {errors.OdometerKm && (
+                      <span className="help text-danger">{errors.OdometerKm}</span>
+                    )}
+                  </CFormLabel>
+                  <CFormInput
+                    name="OdometerKm"
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    onChange={handleChange}
+                    className={`${errors.OdometerKm && 'is-invalid'}`}
+                    size="sm"
+                    id="inputAddress"
+                    placeholder="Km on Odometer"
+                  />
+                </CCol>
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="formFileSm">
+                    Invoice Copy*
+                    {errors.odometerPhoto && (
+                      <span className="help text-danger">{errors.odometerPhoto}</span>
+                    )}
+                  </CFormLabel>
+                  <CFormInput
+                    type="file"
+                    name="odometerPhoto"
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    onChange={handleChange}
+                    value={values.odometerPhoto}
+                    className={`${errors.odometerPhoto && 'is-invalid'}`}
+                    size="sm"
+                    id="formFileSm"
+                  />
+                </CCol>
+              {/* </CRow>
+              <CRow className=""> */}
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">
+                    No. Of Liters*
+                    {errors.OdometerKm && (
+                      <span className="help text-danger">{errors.OdometerKm}</span>
+                    )}
+                  </CFormLabel>
+                  <CFormInput
+                    name="OdometerKm"
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    onChange={handleChange}
+                    className={`${errors.OdometerKm && 'is-invalid'}`}
+                    size="sm"
+                    id="inputAddress"
+                    placeholder="Km on Odometer"
+                  />
+                </CCol>
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">
+                    Rate Per Liter*
+                    {errors.OdometerKm && (
+                      <span className="help text-danger">{errors.OdometerKm}</span>
+                    )}
+                  </CFormLabel>
+                  <CFormInput
+                    name="OdometerKm"
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    onChange={handleChange}
+                    className={`${errors.OdometerKm && 'is-invalid'}`}
+                    size="sm"
+                    id="inputAddress"
+                    placeholder="Km on Odometer"
+                  />
+                </CCol>
+
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Amount</CFormLabel>
+
+                  <CFormInput size="sm" id="inputAddress" value="" readOnly />
+                </CCol>
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="inputAddress">Bunk Reading</CFormLabel>
+
+                  <CFormInput type="file" size="sm" id="inputAddress" value="" />
+                </CCol>
+                <CCol xs={12} md={3}>
+                  <CFormLabel htmlFor="remarks">Remarks*</CFormLabel>
+                  <CFormTextarea
+                    name="remarks"
+                    id="exampleFormControlTextarea1"
+                    rows="1"
+                  ></CFormTextarea>
+                </CCol>
+              </CRow>
+
+              <CRow className="mt-3">
+                <CCol>
+                  <CButton
+                    size="sm"
+                    // disabled={enableSubmit}
+                    color="primary"
+                    className="text-white"
+                    type="submit"
+                  >
+                    Previous
+                  </CButton>
+                </CCol>
+                <CCol className="offset-md-6  d-md-flex justify-content-end" xs={12} sm={12} md={3}>
+                  <CButton
+                    size="sm"
+                    color="warning"
+                    // disabled={enableSubmit}
+                    className="mx-3 text-white"
+                    type="submit"
+                  >
+                    Submit
+                  </CButton>
+                  <CButton
+                    size="sm"
+                    // disabled={enableSubmit}
+                    color="warning"
+                    className="px-3 text-white"
+                    type="submit"
+                  >
+                    Cancel
+                  </CButton>
+                </CCol>
+              </CRow>
+            </CForm>
+          </CTabPane>
+        </CTabContent>
+      </CCard>
+    </>
+  )
+}
+
+export default ParkingYardGate
---
+++ src/components/AppHeader.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  CContainer,
+  CHeader,
+  CHeaderBrand,
+  CHeaderDivider,
+  CHeaderNav,
+  CHeaderToggler,
+  CNavLink,
+  CNavItem,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from '@coreui/icons'
+
+import { AppBreadcrumb } from './index'
+import { AppHeaderDropdown } from './header/index'
+import { logo } from 'src/assets/brand/logo'
+import Title from './Title'
+
+const AppHeader = (props) => {
+  const dispatch = useDispatch()
+  const sidebarShow = useSelector((state) => state.changeState.sidebarShow)
+
+  return (
+    <CHeader position="sticky" className="mb-3 p-0 pt-3">
+      <CContainer fluid>
+        <CHeaderToggler
+          className=""
+          onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
+        >
+          <CIcon icon={cilMenu} size="lg" />
+        </CHeaderToggler>
+        <Title />
+        <CHeaderNav className="ms-3">
+          <AppHeaderDropdown />
+        </CHeaderNav>
+      </CContainer>
+      <CHeaderDivider />
+    </CHeader>
+  )
+}
+export default AppHeader
