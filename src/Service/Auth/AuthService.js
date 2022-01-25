3c4ad421b4cc4f962f2d0a4f218b01534a6a262b
saravanasai <saravanasai002@gmail.com>
2022-01-25 15:19:12 +0530

    logout backend request added & tested

--- src/Service/Auth/AuthService.js
+++ src/Service/Auth/AuthService.js
@@ -1,6 +1,7 @@
 import AppConfig from "src/AppConfig";
 import api from "../Config";
 const AUTH_API_BASE_URL=AppConfig.api.baseUrl+'/admin/login'
+const AUTH_API_BASE_URL_LOGOUT=AppConfig.api.baseUrl+'/admin/logout'
 class AuthService  {
 
     login(data)
@@ -8,7 +9,10 @@ class AuthService  {
       return api.post(AUTH_API_BASE_URL,data)
     }
 
-
+    logout()
+    {
+      return api.post(AUTH_API_BASE_URL_LOGOUT)
+    }
 }
 
 export default new AuthService();
